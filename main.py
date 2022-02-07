8c78014ccf27575767eda7aec93b39bbaee819f8
Seon <76555222+seonjuno0709@users.noreply.github.com>
2022-02-07 15:45:24 +0900

    add def
    add ticket calculator function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from ticket_cal import ticket_calculator
 
 input_hour = input("들어온 시간입력: ")
 input_minute = input("들어온 분입력: ")
@@ -12,25 +13,13 @@ now_time = now_hour*60 + now_min
 
 if 0 <= now_hour < 19: 
     result = now_time - input_time - 60
-    if result < 0:
-        print("시간을 등록할 필요가 없습니다.")
-    elif result == 0:
-        print(f"필요한 주차권의 개수는 1개 입니다.")
-    else:
-        ticket_count = result//30 + 1
-        print(f"필요한 주차권의 개수는 {ticket_count}개 입니다.")
+    ticket_calculator(result)
 
 elif 19<= now_hour < 24:
     # 7시 이후에 들어온 경우
     if 19*60 - input_time < 0:
         result = now_time - input_time - 180
-        if result < 0:
-            print("시간을 등록할 필요가 없습니다.")
-        elif result == 0:
-            print(f"필요한 주차권의 개수는 1개 입니다.")
-        else:
-            ticket_count = result//30 + 1
-            print(f"필요한 주차권의 개수는 {ticket_count}개 입니다.")
+        ticket_calculator(result)
 
     # 7시 이전에 들어온 경우
     elif 19*60 - input_time >= 0:
@@ -39,23 +28,11 @@ elif 19<= now_hour < 24:
         if second_time_section - 180 < 0:
             second_time_section = 0
             result = first_time_section - 60
-            if result < 0:
-                print("시간을 등록할 필요가 없습니다.")
-            elif result == 0:
-                print(f"필요한 주차권의 개수는 1개 입니다.")
-            else:
-                ticket_count = result//30 + 1
-                print(f"필요한 주차권의 개수는 {ticket_count}개 입니다.")
+            ticket_calculator(result)
 
         elif second_time_section - 180 >= 0:
             second_time_section = second_time_section - 180
             result = first_time_section - 60 + second_time_section
-            if result < 0:
-                print("시간을 등록할 필요가 없습니다.")
-            elif result == 0:
-                print(f"필요한 주차권의 개수는 1개 입니다.")
-            else:
-                ticket_count = result//30 + 1
-                print(f"필요한 주차권의 개수는 {ticket_count}개 입니다.")
+            ticket_calculator(result)
       
 
---
+++ ticket_cal.py
@@ -0,0 +1,8 @@
+def ticket_calculator(result):
+  if result < 0:
+    print("시간을 등록할 필요가 없습니다.")
+  elif result == 0:
+    print(f"필요한 주차권의 개수는 1개 입니다.")
+  else:
+    ticket_count = result//30 + 1
+    print(f"필요한 주차권의 개수는 {ticket_count}개 입니다.")
